94df2249c32fd3ba141b0fdf6ce19bd5b0eeb99e
ReshmaMasoodha <reshmamasoodhadgl@gmail.com>
2026-02-02 15:52:34 +0530

    feat: add date navigation with day, month, and year filtering

--- src/ActivityList.jsx
+++ src/ActivityList.jsx
@@ -10,6 +10,11 @@ export const ActivityList = ({activities, onDeleteActivity, onEditActivity, edit
   const handleRadio = (event) => {
     setDateFilter(event.target.value);
   }
+  const handleDate = (event) => {
+    const can1= Number(event.target.value);
+    const can = new Date(viewYear,viewMonth,can1);
+    setViewDate(can);
+  }
   const handleMonth = (event) => {
     setViewMonth(Number(event.target.value));
   }
@@ -59,7 +64,7 @@ export const ActivityList = ({activities, onDeleteActivity, onEditActivity, edit
         },0);
   return(
     <div>
- <ul>
+     <ul>
       <lable>
         <input type="radio"
         value="day"
@@ -73,6 +78,197 @@ export const ActivityList = ({activities, onDeleteActivity, onEditActivity, edit
         value = "month"
         checked={dateFilter=="month"}
         onChange={handleRadio}/> Month
+        <div>
+          {viewMonth == 0||2||4||6||7||9||11 ?
+            <select onChange = {handleDate}>
+          <option value="1">
+            1
+          </option>
+          <option value="2">
+            2
+          </option>
+          <option value="3">
+            3
+          </option>
+          <option value="4">
+            4
+          </option>
+          <option value="5">
+            5
+          </option>
+          <option value="6">
+            6
+          </option>
+          <option value="7">
+            7
+          </option>
+          <option value="8">
+            8
+          </option>
+          <option value="9">
+            9
+          </option>
+          <option value="10">
+            10
+          </option>
+          <option value="11">
+            11
+          </option>
+          <option value="12">
+            12
+          </option>
+          <option value="13">
+            13
+          </option>
+          <option value="14">
+            14
+          </option>
+          <option value="15">
+            15
+          </option>
+          <option value="16">
+            16
+          </option>
+          <option value="17">
+            17
+          </option>
+          <option value="18">
+            18
+          </option>
+          <option value="19">
+            19
+          </option>
+          <option value="20">
+            20
+          </option>
+          <option value="21">
+            21
+          </option>
+          <option value="22">
+            22
+          </option>
+          <option value="23">
+            23
+          </option>
+          <option value="24">
+            24
+          </option>
+          <option value="25">
+            25
+          </option>
+          <option value="26">
+            26
+          </option>
+          <option value="27">
+            27
+          </option>
+          <option value="28">
+            28
+          </option>
+          <option value="29">
+            29
+          </option> 
+          <option value="30"> 
+            30
+          </option>
+          <option value="31">
+            31
+          </option>
+        </select> :
+        <select onChange = {handleDate}>
+          <option value="1">
+            1
+          </option>
+          <option value="2">
+            2
+          </option>
+          <option value="3">
+            3
+          </option>
+          <option value="4">
+            4
+          </option>
+          <option value="5">
+            5
+          </option>
+          <option value="6">
+            6
+          </option>
+          <option value="7">
+            7
+          </option>
+          <option value="8">
+            8
+          </option>
+          <option value="9">
+            9
+          </option>
+          <option value="10">
+            10
+          </option>
+          <option value="11">
+            11
+          </option>
+          <option value="12">
+            12
+          </option>
+          <option value="13">
+            13
+          </option>
+          <option value="14">
+            14
+          </option>
+          <option value="15">
+            15
+          </option>
+          <option value="16">
+            16
+          </option>
+          <option value="17">
+            17
+          </option>
+          <option value="18">
+            18
+          </option>
+          <option value="19">
+            19
+          </option>
+          <option value="20">
+            20
+          </option>
+          <option value="21">
+            21
+          </option>
+          <option value="22">
+            22
+          </option>
+          <option value="23">
+            23
+          </option>
+          <option value="24">
+            24
+          </option>
+          <option value="25">
+            25
+          </option>
+          <option value="26">
+            26
+          </option>
+          <option value="27">
+            27
+          </option>
+          <option value="28">
+            28
+          </option>
+          <option value="29">
+            29
+          </option> 
+          <option value="30"> 
+            30
+          </option>
+        </select>
+          }
+        </div>
         <select onChange = {handleMonth}>
           <option value="0">
             January
@@ -136,7 +332,7 @@ export const ActivityList = ({activities, onDeleteActivity, onEditActivity, edit
           updateActivityItem = {updateAct}
         />)) : <p>No Activities Entered</p>
       }
-    </ul>
+     </ul>
     <p>Total Calories = {totalcalories}</p>
       <p>Total Activities = {filteredActivites.length}</p>
     </div>
